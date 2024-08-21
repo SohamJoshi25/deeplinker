d6993a8c87900b0f0ef68b888efb138c1b236f35
Soham Joshi <sohamjoshichinchwad@gmail.com>
2024-08-21 15:21:26 +0530

    refatored | Github

--- utils/util.deepurl.js
+++ utils/util.deepurl.js
@@ -1,51 +1,41 @@
-const {Packages} = require("./util.data.js");
+const {Packages,PackageName,urlConversionPairs}= require("./util.data.js");
 const { URL } = require('url');
 
-const decodeURL = (Url) => {
-    const urlObj = new URL(Url); // Utilize the URL constructor for accurate parsing
-    
-    const protocol = urlObj.protocol; // Extract protocol directly
-    const host = urlObj.hostname; // Extract the full hostname
-    const port = urlObj.port || (protocol === 'https:' ? '443' : '80'); // Handle default ports
-    
-    // Subdomain extraction logic
-    const hostParts = host.split(".");
-    const subdomain = hostParts.length === 3 ? hostParts[0] : "www";
-    const domain = hostParts.length === 3 ? hostParts[1] : hostParts[0];
-    const tld = hostParts.length === 3 ? hostParts[2] : hostParts[1];
-    const origin = urlObj.origin;
-    const path = urlObj.pathname; // Extract path using URL API
-    
-    return { protocol, host, subdomain, domain, tld, port, path, origin };
-};
 
+function convertShortUrl(url) {
+    for (const shortUrl in urlConversionPairs) {
+        if (url.startsWith(`https://${shortUrl}`) || url.startsWith(`http://${shortUrl}`)) {
+            const longUrl = url.replace(shortUrl, urlConversionPairs[shortUrl]);
+            return longUrl;
+        }
+    }
+    return url;
+}
+ 
+//NOT HANDELLED missing CO-DOMAIN WWW. when db has https://youtube.com :: Handelled for https://youtu.be/dafsf... and https://www.youtube.com/asf..
 const deepURL = (orignalurl) => {
-    if(orignalurl=="")throw new Error("URL cannot be empty");
-    let schemaURL = orignalurl.startsWith("https://")?orignalurl:"https://"+orignalurl;
-    if(schemaURL.includes("youtu.be/")){
-        let temp = schemaURL.split('/');
-        schemaURL = "https://www.youtube.com/watch?v=" + temp[temp.length-1].split("?")[0];
+    if(!orignalurl){
+        throw new Error("URL Not Found | DeeplURL")
     }
 
-    console.log(schemaURL)
+    orignalurl = orignalurl.startsWith("https://")?orignalurl:"https://"+orignalurl;
+    console.log(orignalurl)
+    let url = convertShortUrl(orignalurl);
 
-      const DyamicURL = schemaURL.startsWith("https://")?schemaURL:"https://"+schemaURL
-        const URLObj = decodeURL(DyamicURL);
-        let isPackagePresent = true;
-        const package = Packages[URLObj.domain];
-        if (!package) {
-            isPackagePresent = false;
-        }
+    const URLOBJ = new URL(url);
+    const hostname = URLOBJ.hostname;
 
-        const baseURL = URLObj.host + schemaURL.substring(URLObj.origin.length);
-        const baseHOST = URLObj.domain +"."+ URLObj.tld;
+    const package = PackageName[hostname] || hostname;
+    const path = url.substring(URLOBJ.origin.length); 
+    const appname = hostname.split('.').length>2?hostname.split('.')[1]:hostname.split('.')[0];
+
 
     const result = {
-        package:isPackagePresent,
-        android:`intent://${URLObj.subdomain}.${baseHOST}${schemaURL.substring(URLObj.origin.length)}#Intent;scheme=https;package=${package};end`,
-        ios: `${URLObj.domain}://${baseURL}`,
-        href:"https://" + baseURL
+        android:`intent://${hostname+path}#Intent;scheme=https;package=${package};end`,
+        ios: `${appname}://${hostname+path}`,
+        href:url
     }
+    console.log(result);
     return result;
 }
  
